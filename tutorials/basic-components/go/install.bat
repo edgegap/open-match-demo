f46b8b8d7540a4025970f6b75856e58ebdc796d8
Maxime Boisvert <Maxiimmeb@hotmail.com>
2021-09-14 10:29:38 -0400

    Change install

--- tutorials/basic-components/go/install.bat
+++ tutorials/basic-components/go/install.bat
@@ -1,4 +1,4 @@
 call "%~dp0\scripts\build-images.bat"
-kubectl apply --namespace open-match -f "%~dp0\yaml\open-match-core-1_2_0.yaml"
-kubectl apply --namespace open-match -f "%~dp0\yaml\open-match-override-configmap-1_2_0.yaml" -f "%~dp0\yaml\open-match-default-evaluator-1_2_0.yaml"
-kubectl -n open-match apply -f "%~dp0\yaml\basics.yaml"
+kubectl apply --namespace open-match -f "%~dp0\..\yaml\open-match-core-1_2_0.yaml"
+kubectl apply --namespace open-match -f "%~dp0\..\yaml\open-match-override-configmap-1_2_0.yaml" -f "%~dp0\..\yaml\open-match-default-evaluator-1_2_0.yaml"
+kubectl -n open-match apply -f "%~dp0\..\yaml\basics.yaml"
